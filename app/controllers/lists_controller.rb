2f51cfbb18c6d2d032875121de65afddc4668959
EC2 Default User <ec2-user@ip-172-31-35-2.ap-northeast-1.compute.internal>
2022-10-10 00:34:36 +0000

    [Add] 3-2

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,16 @@
+class ListsController < ApplicationController
+  def new
+    
+  @list = List.new
+  
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- db/migrate/20221008002508_create_lists.rb
+++ db/migrate/20221008002508_create_lists.rb
@@ -1,7 +1,7 @@
 class CreateLists < ActiveRecord::Migration[6.1]
   def change
     create_table :lists do |t|
-      t.string :table
+      t.string :title
       t.string :body
       t.timestamps
     end
